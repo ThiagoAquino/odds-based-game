3a23d9eb61c232192866981743717a882435be6c
Thiago <thiagoaquiino@gmail.com>
2024-10-25 09:58:36 -0300

    update gitignore
    Signed-off-by: Thiago <thiagoaquiino@gmail.com>

---
+++ src/main/kotlin/dto/CreatePlayerRequest.kt
@@ -0,0 +1,17 @@
+package odds.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import org.intellij.lang.annotations.Pattern
+
+data class CreatePlayerRequest(
+
+    @field:NotBlank(message = "Name cannot be blank")
+    val name: String,
+    @field:NotBlank(message = "Surname cannot be blank")
+    val surname: String,
+    @field:NotBlank(message = "Username cannot be blank")
+    @field:Size(min = 3, max = 32, message = "Username must be between 3 and 32 characters")
+    @field:Pattern("^(?!leaderboard$)[a-zA-Z0-9_]{3,32}$")
+    val username: String,
+)
